b1c9ffea6875fda76c2422fa25c3b7bea2ea2f4b
Raul Ionescu <151050993+RIO-20@users.noreply.github.com>
2025-12-14 16:18:11 +0200

    modificare finala

--- pwm_gen.v
+++ pwm_gen.v
@@ -12,7 +12,6 @@ module pwm_gen (
     // top facing signals
     output pwm_out
 );
-    
     reg pwm_out_int;
 
     assign pwm_out = pwm_out_int;
@@ -20,59 +19,65 @@ module pwm_gen (
     always @(posedge clk or negedge rst_n) 
     begin
         if (!rst_n) 
-	begin
+        begin
             pwm_out_int <= 1'b0;
         end
-	else if (!pwm_en) 
-	begin
+        else if (!pwm_en) 
+        begin
+            //Daca PWM nu este activ, iesirea este 0
             pwm_out_int <= 1'b0;
         end
         else
-	begin
-            if (functions == 8'd0) 
-	    begin
-	    		pwm_out_int <= 1'd1;
-                	if (count_val < compare1) 
-			begin
-				pwm_out_int <= 1'd1;
-			end
-			else 
-			begin
-				pwm_out_int <= 1'd0;
-			end
-	    end
-	    else if (functions == 8'd1)
-	    begin
-	    		pwm_out_int <= 1'd0;
-                	if (count_val < compare1) 
-			begin
-				pwm_out_int <= 1'd0;
-			end
-			else 
-			begin
-				pwm_out_int <= 1'd1;
-			end
-	    end
-	    else
-	    begin
-			pwm_out_int <= 0;
-                	if (compare1 < compare2) 
-			begin
-				if (count_val >= compare1 && count_val < compare2)
-				begin
-					pwm_out_int <= 1'd1;
-				end
-				else
-				begin
-					pwm_out_int <= 1'd0;
-				end
+        begin
 
-			end
-			else 
-			begin
-				pwm_out_int <= 1'd1;
-			end
-	    end
-	end
+            if (compare1 == compare2)
+            begin
+                pwm_out_int <= 1'b0;
+            end
+            else if (functions == 8'd0)  //Align Left
+            begin
+                if (count_val <= compare1 && compare1 != 16'd0) 
+                begin
+                    pwm_out_int <= 1'd1;
+                end
+                else 
+                begin
+                    pwm_out_int <= 1'd0;
+                end
+            end
+            else if (functions == 8'd1) //Align Right
+            begin
+                pwm_out_int <= 1'd0;
+                if (count_val < compare1) 
+                begin
+                    pwm_out_int <= 1'd0;
+                end
+                else 
+                begin
+                    pwm_out_int <= 1'd1;
+                end
+            end
+            else // Range (intre doua valori) 
+            begin
+                
+                if (compare1 < compare2) 
+                begin
+                    //Activ intre compare1 si compare2
+                    if (count_val >= compare1 && count_val < compare2)
+                    begin
+                        pwm_out_int <= 1'd1;
+                    end
+                    else
+                    begin
+                        pwm_out_int <= 1'd0;
+                    end
+                end
+                else 
+                begin
+                    //Ramene 1 daca e configuratie invalida (c1 > c2)
+                    pwm_out_int <= 1'd1;
+                end
+            end
+        end
     end
 endmodule
